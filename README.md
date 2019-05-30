021d7c0d5551a669db484da87d3bb2421a6cb3e9
ownsupernoob2 <46392586+ownsupernoob2@users.noreply.github.com>
2019-05-30 18:11:21 +0700

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ahmedfuad
-Created with CodeSandbox
---
+++ ignore/index.js
@@ -0,0 +1,5 @@
+import "sanitize.css";
+import "../style/style.scss";
+import "../style/style-about.scss";
+import "../style/style-contact.scss";
+import "../style/burger.scss";
